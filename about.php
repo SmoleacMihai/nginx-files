b7f47a522d70c5707e84d7d65506f373aef87847
SmoleacMihai <mihaismoleac2002@gmail.com>
2022-10-31 14:18:17 +0200

    h

--- about.php
+++ about.php
@@ -17,23 +17,10 @@
     <div class="scroll-up-btn">
         <i class="fas fa-angle-up"></i>
     </div>
-    <nav class="navbar sticky">
-        <div class="max-width">
-            <div class="logo"><a href="./index.php"><span>My</span>Portfolio</a></div>
-            <ul class="menu">
-                <li><a href="./index.php" class="menuBtn">Home</a></li>
-                <li><a href="./about.php" class="menuBtn">About</a></li>
-                <li><a href="./services.php" class="menuBtn">Services</a></li>
-                <li><a href="./gallery.php" class="menuBtn">Gallery</a></li>
-                <li><a href="./team.php" class="menuBtn">Team</a></li>
-                <li><a href="./project.php" class="menuBtn">Projects</a></li>
-                <li><a href="./contact.php" class="menuBtn">Contact</a></li>
-            </ul>
-            <div class="menuBtn">
-                <i class="fas fa-bars" id="menu-btn"></i>
-            </div>
-        </div>
-    </nav>
+    <?php
+        include "./components/NavBar.component.php";
+        echo NavBar();
+    ?> 
     <section class="about" id="about">
         <div class="max-width">
             <h2 class="title">About me</h2>
